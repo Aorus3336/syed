a8c0cb4dd2f01440f3680863204e5a7480937fb1
Aorus3336 <aorus3336@gmail.com>
2024-11-22 15:29:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+
+BOT_TOKEN=("8175435417:AAFevvyqscM5PYjwVPazppnqEPChAlKt0UY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@SAYYED_Z")
+
+ADMIN_IDS=("1713986516 , 5134043595")
+
+
+
+
+
+
+
+  
